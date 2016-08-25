530d679841b19fe0d6fd1e2b4a91b1e3c4793ad4
kgh955 <kgh955@naver.com>
2016-08-25 21:57:49 +0900

    .

--- 01_menu/app/src/main/java/com/example/b/a01_menu/MainActivity.java
+++ 01_menu/app/src/main/java/com/example/b/a01_menu/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.b.a01_menu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +15,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        registerForContextMenu(textView);
         // hello world test
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.add(0,100, 0, "context menu1");
+        menu.add(0,101, 0, "context menu2");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getItemId() == 100) {
+            Toast.makeText(MainActivity.this, "context 1 selected", Toast.LENGTH_SHORT).show();
+        }else if(item.getItemId() == 101) {
+            Toast.makeText(MainActivity.this, "context 2 selected", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_test1) {
+            Toast.makeText(MainActivity.this, "menu 1 selected", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
+
+
 }
+
---
+++ 02_widget/app/src/main/java/com/example/b/a02_widget/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.b.a02_widget;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText myEdit = (EditText)findViewById(R.id.myEdit);
+        Button btnHello = (Button)findViewById(R.id.btnHello);
+        btnHello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.w("Widget", "button clicked!!!");
+                String str = myEdit.getText().toString();
+                Toast.makeText(MainActivity.this, myEdit.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final CheckBox myCheckBox = (CheckBox)findViewById(R.id.myCheckBox);
+        myCheckBox.setChecked(true);
+        myCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    Toast.makeText(MainActivity.this, "checked", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "unchecked", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        RadioGroup myRadioGroup = (RadioGroup)findViewById(R.id.myRadioGroup);
+        myRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.myRadio1:
+                        Toast.makeText(MainActivity.this, "radio1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.myRadio2:
+                        Toast.makeText(MainActivity.this, "radio2", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.myRadio3:
+                        Toast.makeText(MainActivity.this, "radio3", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+    }
+}
