51c0eff33bf41a7965f029d5210dcc04926910ab
kgh955 <kgh955@naver.com>
2016-08-26 21:53:36 +0900

    ..count 100.. thread2 ...

--- 06_thread/app/src/main/java/com/example/b/a06_thread/MainActivity.java
+++ 06_thread/app/src/main/java/com/example/b/a06_thread/MainActivity.java
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     class MyThread extends Thread{
         @Override
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 Log.d("Mythread", "count : "+i);
+                btn.setText("count : "+i);
             }
         }
     }
@@ -25,11 +29,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
     }
 
     public void onBtnClick(View v){
-        MyThread th = new MyThread();
-        th.start();
+//        MyThread th = new MyThread();
+//        th.start();
+
+        Thread th2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i<100; i++){
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Log.d("Mythread", "count : "+i);
+                    btn.setText("count : "+i);
+                }
+            }
+        });
+        th2.start();
     }
 
 }
