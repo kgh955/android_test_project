4ba90b0b6735561b51d083a12c6319629a97d04e
kgh955 <kgh955@naver.com>
2016-08-26 21:40:34 +0900

    ..count 100..

---
+++ 06_thread/app/src/main/java/com/example/b/a06_thread/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.b.a06_thread;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    class MyThread extends Thread{
+        @Override
+        public void run() {
+            for(int i = 0; i<100; i++){
+                try {
+                    sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.d("Mythread", "count : "+i);
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View v){
+        MyThread th = new MyThread();
+        th.start();
+    }
+
+}
