d810a7eea96abeffd8a27a8fcf9b48b7e829bbd1
kgh955 <kgh955@naver.com>
2016-08-29 20:57:54 +0900

    asynctask : add async msg

--- 07_asynctask/app/src/main/java/com/example/b/a07_asynctask/MyAsyncTask.java
+++ 07_asynctask/app/src/main/java/com/example/b/a07_asynctask/MyAsyncTask.java
@@ -1,16 +1,21 @@
 package com.example.b.a07_asynctask;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by b on 2016-08-29.
+ */
+class  MyAsyncTask extends AsyncTask<Integer, Integer, String> {
 
-    TextView textView;
-/*
-    class  MyAsyncTask extends AsyncTask<Integer, Integer, String>{
+        TextView textView;
 
-        @Override // alt + insert
+    public MyAsyncTask(TextView textView) {
+        this.textView = textView;
+    }
+
+    @Override // alt + insert
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
             textView.setText("count : "+values[0]);
@@ -19,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-             textView.setText(s);
+            textView.setText(s);
         }
 
         @Override
@@ -39,25 +44,12 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 Log.d("Asynctask", "count : "+i);
-            // textView.setText("count : "+i);
+                // textView.setText("count : "+i);
                 publishProgress(i);
 
             }
 
             return "Async done!!!";
         }
-    }
-*/
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        textView = (TextView) findViewById (R.id.textView);
-
-        MyAsyncTask task = new MyAsyncTask(textView);
-        task.execute(10);
-
-    }
-
 }
+
