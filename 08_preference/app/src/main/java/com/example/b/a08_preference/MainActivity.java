0b82321966a50bb222e74f88317feb2e6031a0ef
kgh955 <kgh955@naver.com>
2016-08-29 21:23:38 +0900

    08_shared preference : msg output

--- 08_preference/app/src/main/java/com/example/b/a08_preference/MainActivity.java
+++ 08_preference/app/src/main/java/com/example/b/a08_preference/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.b.a08_preference;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences pref = getSharedPreferences("MySettings", 0);
+        String str = pref.getString("id", " NO DATA");
+        Toast.makeText(MainActivity.this, "str : "+str, Toast.LENGTH_SHORT).show();
+
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("id", "abc");
+
+        //
+        editor.commit(); // save to 'pref'
     }
 
 }
