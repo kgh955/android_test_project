0c55f9516333d7965ddbd15a749d4527a3137a3f
kgh955 <kgh955@naver.com>
2016-08-31 21:41:34 +0900

    ..10_json : done

--- 10_json/app/src/main/java/com/example/b/a10_json/MainActivity.java
+++ 10_json/app/src/main/java/com/example/b/a10_json/MainActivity.java
@@ -1,13 +1,69 @@
 package com.example.b.a10_json;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    String str = "[ {'name':'kim', 'tel':'010-1111-2222'}," +
+                    "{'name':'park', 'tel':'010-3333-4444'}," +
+                    "{'name':'lee', 'tel':'010-5555-6666'}]";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        localJsonTest();
+        internetJsonTest();
+
+    }
+
+    private void internetJsonTest(){
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://api.androidhive.info/contacts/", new JsonHttpResponseHandler(){
+
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+
+                try {
+                    JSONArray array = response.getJSONArray("contacts");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                Log.d("contacts", response.toString());
+            }
+
+        });
+    }
+
+    private void localJsonTest() {
+        try {
+            JSONArray array = new JSONArray(str);
+            for(int i= 0; i< array.length(); i++){
+
+                JSONObject obj = array.getJSONObject(i);
+                String name = obj.getString("name");
+                String tel = obj.getString("tel");
+                Log.d("json", name + " "+ tel);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
