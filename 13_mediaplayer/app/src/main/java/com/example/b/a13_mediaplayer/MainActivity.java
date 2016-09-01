786ab230a7782ea29c35f6357817212459beac6e
kgh955 <kgh955@naver.com>
2016-09-01 21:52:10 +0900

    13_mediaplayer : on adding path..

--- 13_mediaplayer/app/src/main/java/com/example/b/a13_mediaplayer/MainActivity.java
+++ 13_mediaplayer/app/src/main/java/com/example/b/a13_mediaplayer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.b.a13_mediaplayer;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onPlayClick(View v){
+
+        String path = Environment.getDownloadCacheDirectory().toString();
+
+    }
+
+
+
 }
