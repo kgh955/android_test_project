158b2bb22b1c1a559de599b50fed167c83c5b9f2
kgh955 <kgh955@naver.com>
2016-09-01 20:03:29 +0900

    11_webview : going...

--- 11_webview/app/src/main/java/com/example/b/a11_webview/MainActivity.java
+++ 11_webview/app/src/main/java/com/example/b/a11_webview/MainActivity.java
@@ -1,13 +1,68 @@
 package com.example.b.a11_webview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    WebView webView;
+    ProgressDialog dlg;
+    ProgressBar progressBar;
+
+    class MyWebViewClient extends WebViewClient{
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            dlg.show();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            dlg.dismiss();
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+    class MyWebChromeClient extends WebChromeClient{
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+            progressBar.setProgress(newProgress);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        dlg = new ProgressDialog(this);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings ws = webView.getSettings();
+        ws.setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.naver.com");
+
     }
+
+    public void onBtnClick(View w){
+        EditText editURL = (EditText) findViewById(R.id.editURL);
+        webView.loadUrl(editURL.getText().toString());
+    }
+
 }
