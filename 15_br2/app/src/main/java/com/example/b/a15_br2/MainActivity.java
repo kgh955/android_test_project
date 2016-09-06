5147be46d82d4b0a0001248cf40f3e1d04d5c94b
peter <kgh955@naver.com>
2016-09-06 19:31:28 +0900

    15_br2 : add some receive...

--- 15_br2/app/src/main/java/com/example/b/a15_br2/MainActivity.java
+++ 15_br2/app/src/main/java/com/example/b/a15_br2/MainActivity.java
@@ -14,9 +14,16 @@ public class MainActivity extends AppCompatActivity {
     BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-            Toast.makeText(context, "batt : "+level, Toast.LENGTH_LONG).show();
-        }
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_BATTERY_CHANGED)) {
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+                Toast.makeText(context, "batt : "+level, Toast.LENGTH_LONG).show();
+            }else if(action.equals(Intent.ACTION_BATTERY_LOW)){
+
+            } else if(action.equals("abcdef")){
+                Toast.makeText(context, "custom action", Toast.LENGTH_LONG).show();
+            }
+         }
     };
 
     @Override
@@ -30,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        filter.addAction(Intent.ACTION_BATTERY_LOW);
         registerReceiver(receiver, filter);
     }
 
@@ -39,5 +47,12 @@ public class MainActivity extends AppCompatActivity {
 
         unregisterReceiver(receiver);
     }
+
+    public void onSendClick(VIew v){
+        Intent intent = new Intent("abcedf");
+        sendBroadcast(intent);
+    }
+
+
 
 }
