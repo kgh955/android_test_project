68aa220d524a98dae78bd236daa38085d0441b3f
peter <kgh955@naver.com>
2016-09-08 19:10:42 +0900

    21_dialogue : start

---
+++ 21_dialogue/app/src/main/java/com/example/kihwani/a21_dialogue/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.kihwani.a21_dialogue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View v) {
+
+
+    }
+}
